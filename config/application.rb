3f061f69b3dfa0cc984c1d3e6f1a8412cc71119f
NicoH <n.hermet@gmail.com>
2021-09-03 16:22:19 +0200

    feat(database ids): replaces ids by uuids
    BREAKING CHANGE: uuids are set up
    
    Closes #39

--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,10 @@ module FullTrackDev
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    # Config UUID
+    config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+    end
   end
 end
