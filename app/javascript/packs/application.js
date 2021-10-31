636e74d82a1f87bfeddcc21969586d61cc62120d
NicoH <n.hermet@gmail.com>
2021-10-31 18:06:48 +0100

    feat(button): change font size and CTA

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import 'styles/navbar'
+import 'styles/button'
 import 'fonts/Montserrat'
 
 Rails.start()
