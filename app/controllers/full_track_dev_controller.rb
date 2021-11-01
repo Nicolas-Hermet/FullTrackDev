6bb427518cfdade1b4ab20a33a65010440fb7f23
NicoH <n.hermet@gmail.com>
2021-11-01 17:00:56 +0100

    Creates a dedicated layout for the landing page

--- app/controllers/full_track_dev_controller.rb
+++ app/controllers/full_track_dev_controller.rb
@@ -1,11 +1,11 @@
 class FullTrackDevController < ApplicationController
+  layout "landing", only: :landing
+
   def home; end
 
   def contact; end
 
   def wip; end
 
-  def landing
-    render layout: false
-  end
+  def landing; end
 end
