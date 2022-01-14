88722420935117cc4f9723aa7da68d175577870d
Nicolas Hermet <38986312+Nicolas-Hermet@users.noreply.github.com>
2022-01-14 16:47:07 +0100

    Install devise for admin (#52)
    * Force SSL connections
    
    * Install devise
    
    * Generate devise for Admin
    
    It is not registrable nor validatable
    
    * Generate views for devise
    
    * Add one admin in development and test environment
    
    * Dress devise sign_in page
    
    * Add sign_out button to navbar if admin signed_in

--- Gemfile
+++ Gemfile
@@ -34,6 +34,9 @@ gem "letter_opener"
 # SendinBlue API
 gem 'sib-api-v3-sdk'
 
+# Devise
+gem 'devise'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,6 +26,7 @@ Rails.application.configure do
     :authentication => 'login',
     :enable_starttls_auto => true
   }
+  config.action_mailer.default_url_options = { :host => 'full-track-dev.herokuapp.com' }
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
@@ -57,7 +58,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
