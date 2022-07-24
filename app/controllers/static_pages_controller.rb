f89674e7df93286bb1d0f59986fc123566cb16df
NicoH <n.hermet@gmail.com>
2022-07-24 11:34:13 +0200

    Add embedded image in article banners

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,9 @@
 class StaticPagesController < ApplicationController
   def landing; end
 
-  def home; end
+  def home
+    @articles = Article.where(status: :published).with_rich_text_content_and_embeds.sample(3)
+  end
 
   def contact
     ContactFormMailer.with(params).message_email.deliver_now
