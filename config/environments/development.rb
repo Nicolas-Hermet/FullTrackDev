e4231fd8c392d86b25b73ad88aea94b2345779a3
NicoH <n.hermet@gmail.com>
2022-08-11 12:15:26 +0200

    Add image processing gem to deal with blob representable

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,6 +32,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
+  config.active_storage.variable_content_types = %w(image/png image/gif image/jpg image/jpeg image/pjpeg image/tiff image/bmp image/vnd.adobe.photoshop image/vnd.microsoft.icon image/webp image/heic)
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
