bd2cbab175c489dda87731446f1f719f7f2be15d
NicoH <n.hermet@gmail.com>
2022-08-16 08:31:01 +0200

    Fix typo in file name

--- app/controllers/embeds_controller.rb
+++ app/controllers/embeds_controller.rb
@@ -1,6 +1,7 @@
 require 'oembed'
 
 class EmbedsController < ApplicationController
+  # protect_from_forgery
   def index
     begin
       @embed = Embed.find_or_create_by(url: params[:url])
@@ -9,7 +10,6 @@ class EmbedsController < ApplicationController
             partial: @embed.to_trix_content_attachment_partial_path,
             locals: { @embed.class.name.underscore.to_sym => @embed }
           )
-
         render json: { sgid: @embed.attachable_sgid, content: content }
       else
         render json: {}, status: :unprocessable_entity
