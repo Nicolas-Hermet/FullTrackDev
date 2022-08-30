5bffa4a79660bbb0883a1d170cc592479068cf00
NicoH <n.hermet@gmail.com>
2022-08-30 07:35:52 +0200

    Remove condition on articles count

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ require 'net/https'
 
 class ApplicationController < ActionController::Base
   add_flash_types :danger, :info, :warning, :success, :messages
-  before_action :count_articles
 
   RECAPTCHA_MINIMUM_SCORE = 0.5
 
@@ -13,11 +12,5 @@ class ApplicationController < ActionController::Base
     response = Net::HTTP.get_response(uri)
     json = JSON.parse(response.body)
     json['success'] && json['score'] > RECAPTCHA_MINIMUM_SCORE && json['action'] == recaptcha_action
-  end
-
-  private
-
-  def count_articles
-    @article_count = Article.where(status: :published).count
   end
 end
