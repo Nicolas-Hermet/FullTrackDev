cfc2429383e7198f099a5a44988cea9d949ac1b8
NicoH <n.hermet@gmail.com>
2022-09-26 10:55:10 +0200

    Fix mailer and mailer specs

--- spec/mailers/contact_form_spec.rb
+++ spec/mailers/contact_form_spec.rb
@@ -1,5 +1,17 @@
 require "rails_helper"
 
 RSpec.describe ContactFormMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:message) { 'Hello there !' }
+  let(:params) do
+    { name: 'visitor', visitor: 'visitor@example.com', message: message }
+  end
+  let(:mail) { described_class.with(params).message_email }
+
+  it 'sends the correct information' do
+    # allow(:params).to receive(:name).and_return('visitor')
+    expect(mail.to).to eq(['nico@fulltrack.dev'])
+    expect(mail.from).to eq(['no-reply@fulltrack.dev'])
+    expect(mail.subject).to include('has a question')
+    expect(mail.body).to include(message)
+  end
 end
