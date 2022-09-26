0e7846630ce10b437fbbf24644c96ea2b31b16fa
NicoH <n.hermet@gmail.com>
2022-09-26 10:58:15 +0200

    Spec article model validations

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:article_attributes) { attributes_for(:article) }
+
+  %i[title category status].each do |argument|
+    it "validates presence of #{argument}" do
+      article_attributes[argument] = nil
+      expect(Article.new(article_attributes)).not_to be_valid
+    end
+  end
+
 end
