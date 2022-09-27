3eddfd850ded6fe7a35768ae359be7159d1b3101
NicoH <n.hermet@gmail.com>
2022-09-27 19:33:32 +0200

    Add time travelling from ActiveSupport

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -66,4 +66,7 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   config.include Devise::Test::IntegrationHelpers, type: :request
   config.include Devise::Test::ControllerHelpers, type: :view
+
+  # Time travelling
+  config.include ActiveSupport::Testing::TimeHelpers
 end
