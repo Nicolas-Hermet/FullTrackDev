b9eac380abf4f07aba056d0fbb215cd345e27352
NicoH <n.hermet@gmail.com>
2022-09-27 19:34:19 +0200

    Add specifications for article update

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'active_support/testing/time_helpers'
 
 RSpec.describe Article, type: :model do
   let(:article_attributes) { attributes_for(:article) }
@@ -10,4 +11,44 @@ RSpec.describe Article, type: :model do
     end
   end
 
+  describe 'update' do
+    let(:article) { create(:article, status: 'draft', published_at: nil) }
+    let(:update_params) do
+      { title: Faker::Book.title, status: 'draft' }
+    end
+
+    context 'when arguments contain status: "draft"' do
+      it 'does not update the "published_at" attribute' do
+        article.update(update_params)
+        expect(article.reload.published_at).to be_nil
+      end
+    end
+
+    context 'when arguments contain status: "published"' do
+      context 'when article already had a published status or a published_at dateTime' do
+        let(:date) { '1986-02-15' }
+        let(:possible_attributes) do
+          [{ status: 'published' }, { published_at: date }]
+        end
+        let(:random_attribute) { possible_attributes.sample }
+        let(:article) { create(:article, random_attribute) }
+        let(:article_published_date) { article.published_at }
+
+        it 'does not update "published_at" attribute' do
+          article.update(update_params)
+          expect(article.reload.published_at).to eq(article_published_date)
+        end
+      end
+
+      context 'when article had status: "draft" and "published_at: nil"' do
+        it 'does update the published_at attribute with Time.now' do
+          update_params[:status] = 'published'
+          freeze_time
+          article.update(update_params)
+          expect(article.reload.published_at).to eq(Time.now)
+          unfreeze_time
+        end
+      end
+    end
+  end
 end
