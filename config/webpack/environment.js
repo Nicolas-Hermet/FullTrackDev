6087470cc125d58779823d5a2ae4bd0ad1b4082a
Nicolas Hermet <nicolas.hermet@lisaia.com>
2025-08-11 21:52:54 +0200

    Fix css
    ahah... who am I kidding seriously

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,10 @@
 const { environment } = require('shakapacker')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-// Ensure Babel transpiles our JS (including class features plugins configured in babel.config.js)
+// In production, extract CSS to standalone files. In dev, inject via style-loader.
+const isProd = process.env.NODE_ENV === 'production'
+
+// --- JavaScript (unchanged from your setup) ---
 environment.loaders.append('babel', {
   test: /\.m?js$/,
   exclude: /node_modules/,
@@ -8,5 +12,40 @@ environment.loaders.append('babel', {
     loader: 'babel-loader'
   }
 })
+
+// --- CSS ---
+const cssRule = {
+  test: /\.css$/i,
+  use: [
+    isProd ? MiniCssExtractPlugin.loader : 'style-loader',
+    { loader: 'css-loader', options: { importLoaders: 1 } },
+    'postcss-loader'
+  ]
+}
+
+// --- SCSS/SASS ---
+const scssRule = {
+  test: /\.(scss|sass)$/i,
+  use: [
+    isProd ? MiniCssExtractPlugin.loader : 'style-loader',
+    { loader: 'css-loader', options: { importLoaders: 2 } },
+    'postcss-loader',
+    'sass-loader'
+  ]
+}
+
+environment.loaders.prepend('css', cssRule)
+environment.loaders.prepend('scss', scssRule)
+
+// --- Plugins ---
+if (isProd) {
+  environment.plugins.prepend(
+    'MiniCssExtractPlugin',
+    new MiniCssExtractPlugin({
+      filename: 'css/[name]-[contenthash].css',
+      chunkFilename: 'css/[id]-[contenthash].css'
+    })
+  )
+}
 
 module.exports = environment
