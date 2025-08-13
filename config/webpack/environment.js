2d36d62e13f549a97826255ed75f90e2bd51c930
Nicolas Hermet <nicolas.hermet@lisaia.com>
2025-08-13 17:05:05 +0200

    Remove redundant CSS rules

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,53 +1,10 @@
-const { environment } = require('shakapacker')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+// See the shakacode/shakapacker README and docs directory for advice on customizing your webpackConfig.
+const { generateWebpackConfig } = require('shakapacker')
+const { merge } = require('webpack-merge')
 
-const isProd =
-  process.env.NODE_ENV === 'production' ||
-  process.env.RAILS_ENV === 'production' ||
-  process.env.SHAKAPACKER_ENV === 'production';
-
-try { environment.loaders.delete('sass') } catch (e) {}
-try { environment.loaders.delete('scss') } catch (e) {}
-try { environment.loaders.delete('css') } catch (e) {}
-
-environment.loaders.append('babel', {
-  test: /\.m?js$/,
-  exclude: /node_modules/,
-  use: { loader: 'babel-loader' }
+// Do NOT redefine CSS/SCSS rules here. Those are configured in config/webpack/environment.js
+module.exports = merge(generateWebpackConfig(), {
+  resolve: {
+    extensions: ['.js', '.mjs', '.scss', '.css']
+  }
 })
-
-// --- CSS ---
-const cssRule = {
-  test: /\.css$/i,
-  use: [
-    isProd ? MiniCssExtractPlugin.loader : 'style-loader',
-    { loader: 'css-loader', options: { importLoaders: 1 } },
-    'postcss-loader'
-  ]
-}
-
-// --- SCSS/SASS ---
-const scssRule = {
-  test: /\.(scss|sass)$/i,
-  use: [
-    isProd ? MiniCssExtractPlugin.loader : 'style-loader',
-    { loader: 'css-loader', options: { importLoaders: 2 } },
-    'postcss-loader',
-    'sass-loader'
-  ]
-}
-
-environment.loaders.append('css', cssRule)
-environment.loaders.append('scss', scssRule)
-
-if (isProd) {
-  environment.plugins.prepend(
-    'MiniCssExtractPlugin',
-    new MiniCssExtractPlugin({
-      filename: 'css/[name]-[contenthash].css',
-      chunkFilename: 'css/[id]-[contenthash].css'
-    })
-  )
-}
-
-module.exports = environment
--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -2,24 +2,9 @@
 const { generateWebpackConfig } = require('shakapacker')
 const { merge } = require('webpack-merge')
 
-// Add explicit support for .css and .scss so imports like '../css/application.scss' work reliably
-const customConfig = {
-  module: {
-    rules: [
-      {
-        test: /\.s?css$/i,
-        use: [
-          // Order matters: inject styles, resolve CSS, then compile SASS
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
-      }
-    ]
-  },
+// Do NOT redefine CSS/SCSS rules here. Those are configured in config/webpack/environment.js
+module.exports = merge(generateWebpackConfig(), {
   resolve: {
     extensions: ['.js', '.mjs', '.scss', '.css']
   }
-}
-
-module.exports = merge(generateWebpackConfig(), customConfig)
+})
